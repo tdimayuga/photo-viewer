0ba5d58b2376122b661d8aff7cd4edb0bf87e15c
tdimayuga <tc.dimayuga@gmail.com>
2021-09-08 00:00:42 -0400

    Display PostCreator in authenticated users profile

--- pages/user-profile/[id].js
+++ pages/user-profile/[id].js
@@ -4,39 +4,47 @@ import Feed from '../../shared/Feed'
 import Header from '../../components/Header'
 import useToken from '../../components/useToken'
 import { getPostsByUser } from '../api/PostsApi'
-import { getUserInfoById } from '../api/UsersApi'
+import { getUserInfo, getUserInfoById } from '../api/UsersApi'
 import PostCreator from '../../shared/PostCreator'
 
 const UserProfile = () => {
   const router = useRouter()
   const { id } = router.query
-  const [profileData, setProfileData] = useState({profileInfo: [], profilePosts: []})
-  // const [profileInfo, setProfileInfo] = useState()
-  // const [profilePosts, setProfilePosts] = useState()
+  const [profileData, setProfileData] = useState({
+    profileInfo: [],
+    profilePosts: [],
+  })
+  const [user, setUser] = useState('')
   const { token, setToken } = useToken()
   const isLoggedIn = token
-  const { profileInfo, profilePosts} = profileData
+  const { profileInfo, profilePosts } = profileData
+  const { name: profileName } = profileInfo
+  const { name, id: userId } = user
 
   useEffect(() => {
     if (!router.isReady) return
     !isLoggedIn ? router.push('/') : loadUserProfileInfo()
-  }, [router.isReady, token,])
+  }, [router.isReady, token, id])
 
   const loadUserProfileInfo = async () => {
-    const userInfo = await getUserInfoById(id)
-    const userPosts = await getPostsByUser(id)
+    const userInfo = await getUserInfo(token)
+    const profileInfo = await getUserInfoById(id)
+    const profilePosts = await getPostsByUser(id)
 
-    setProfileData({profileInfo: userInfo,
-      profilePosts: userPosts})
+    setUser(Object.assign({}, ...userInfo))
+    setProfileData({
+      profileInfo: Object.assign({}, ...profileInfo),
+      profilePosts: profilePosts,
+    })
   }
 
   return (
     <>
-      {isLoggedIn && !!profileInfo && !!profilePosts &&(
+      {isLoggedIn && !!profileInfo && !!profilePosts && user && (
         <>
-          <Header setToken={setToken} id={id} />
-          {profileInfo && <h2>Hello {profileInfo[0]?.name}</h2>}
-          <PostCreator userId={id} name={profileInfo[0]?.name} />
+          <Header setToken={setToken} id={userId} />
+          {profileInfo && <h2>Hello {profileName}</h2>}
+          {userId == id && <PostCreator userId={userId} name={name} />}
           <Feed posts={profilePosts} />
         </>
       )}
